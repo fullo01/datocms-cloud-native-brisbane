adfe6707853e8e1c7509ca9bc271040c2b7f0ad1
Andrew Fullerton <ae.fullerton@gmail.com>
2022-12-21 21:42:20 +1000

    updated styling

--- src/components/intro.js
+++ src/components/intro.js
@@ -3,16 +3,16 @@ import React from "react";
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-3xl md:text-xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h1 className="text-5xl md:text-xl font-bold tracking-tighter leading-tight md:pr-8">
         Blog by Cloud Collective Brisbane
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        Return{' '}
+        Return to{' '}
         <a
           href="https://www.cloudcollectivebrisbane.com/"
           className="underline hover:text-success duration-200 transition-colors"
         >
-          to homepage 
+          homepage 
         </a>{' '}
       </h4> 
     </section>
